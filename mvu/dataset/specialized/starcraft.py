e4ab8fac09e16ee23986232a92d43129b9e2d898
David Burnett <david.b.laughs@gmail.com>
2024-02-29 21:27:28 -0500

    Rename starcraft max_samples to samples
    Simplier name mostly, but also may use it for speciic samples in the future

--- mvu/dataset/specialized/starcraft.py
+++ mvu/dataset/specialized/starcraft.py
@@ -46,14 +46,14 @@ class StarCraftDataset(Dataset[Tuple[Tensor, Tensor]]):
 
 
 def createStarCraftDataset(path: str = None, targets: Union[str, List[str]] = None,
-                           validation_percent: float = 0.3, max_samples: Dict[str,int] = None,
+                           validation_percent: float = 0.3, samples: Dict[str,int] = None,
                            image_size: int = 64, sensor_size: int = 1) -> TorchDatasetSplits:
     """
     Creates the needed objects to use the starcraft dataset
     :param path:                Location to load the starcraft dataset into
     :param targets:             Fields from metadata to use as the regression target, can be a string or list of strings
     :param validation_percent:  Percentage of training data to use for validation
-    :param max_samples:         Maximum samples from the dataset to use for train, validate, and test.
+    :param samples:             Maximum samples from the dataset to use for train, validate, and test.
     :param image_size:          Size of the image in pixels
     :param sensor_size:         Size of sensors for making values missing
     :return:  Dataset instance
@@ -84,18 +84,18 @@ def createStarCraftDataset(path: str = None, targets: Union[str, List[str]] = No
     validationEnd = int(trainingValidationSize * validation_percent)
     trainingStart = validationEnd
     trainingEnd = trainingValidationSize
-    if max_samples is not None:
+    if samples is not None:
         # training has a start offset and an end, so need some math to convert the limit
-        if "train" in max_samples:
-            trainLimit = max_samples["test"]
+        if "train" in samples:
+            trainLimit = samples["test"]
             if trainLimit < trainingEnd - trainingStart:
                 trainingEnd = trainingStart + trainLimit
         # validate is easy to limit, just reduce max samples
-        if "validate" in max_samples:
-            validationEnd = min(validationEnd, max_samples["validate"])
+        if "validate" in samples:
+            validationEnd = min(validationEnd, samples["validate"])
         # only make test a subset if needed, can use the raw dataset otherwise
-        if "test" in max_samples:
-            testLimit = max_samples["test"]
+        if "test" in samples:
+            testLimit = samples["test"]
             if testLimit < len(testing):
                 testing = Subset(testing, range(0, testLimit))
     # apply the computed limits
