954d1212e524663f78f628c8f146c8e64428f310
David Burnett <david.b.laughs@gmail.com>
2024-08-28 19:40:38 -0400

    Added training script for CelebA on a neural network

---
+++ scripts/learn_neural_network_celeba.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Loads in all relevant datasets
+source ../../miniconda/bin/activate
+conda activate ./venv
+
+python learn_neural_network.py celeba --seed 1337 --output ./models/celeba6/ \
+    --validate_every 10 --training_iterations 1000 --batch_size 250 --patience 5 \
+    --architecture '{"name": "resnet"}' \
+    '{
+      "path": "../../datasets/CelebAMask/256/img",
+      "lists_root": "datasets/celeba",
+      "attributes_path": "../../datasets/CelebAMask/1024/CelebAMask-HQ-attribute-anno.txt"
+    }'
+# resnet options:
+#  momentum
+#  track_running_stats
+#  pretrained_weights
+#    --architecture '{"name": "resnet", "momentum": 0.01, "track_running_stats": false}' \
