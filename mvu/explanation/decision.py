30a84c93cc8e7e433a7ecc7c68c75c0c79c7106c
David Burnett <david.b.laughs@gmail.com>
2024-10-15 16:03:40 -0400

    Improve decision making methods
    Instead of computing the probability that an action is dominated, compute the probability it dominates, which we call confidence
    Use confidence to compute the best action

--- mvu/explanation/decision.py
+++ mvu/explanation/decision.py
@@ -1,16 +1,18 @@
+from typing import Tuple
+
 import torch
 from torch import Tensor
 from torch.distributions import Distribution
 
 
-def computeProbabilityActionDominated(phi: Tensor, lossFunction: callable, action: int, actions: Tensor) -> float:
+def computeActionConfidence(phi: Tensor, lossFunction: callable, action: int, actions: Tensor) -> float:
     """
-    Computes the probability that the given action is dominated by another action.
+    Computes the probability that the given action dominates all other actions.
     :param phi:           Samples from the phi distribution
     :param lossFunction:  Loss function, first parameter is the Y label (0 or 1) and second is the action tensor
     :param action:        Primary action to consider
     :param actions:       Tensor of all possible actions
-    :return:  Probability that action is dominated by another action
+    :return:  Probability that action dominates all other actions
     """
     # compute loss for each action, outer product makes dimensions [sampleIdx, actionIdx]
     allLoss = torch.outer(phi, lossFunction(1, actions)) + torch.outer(1 - phi, lossFunction(0, actions))
@@ -18,15 +20,68 @@ def computeProbabilityActionDominated(phi: Tensor, lossFunction: callable, actio
     # TODO: I feel like there should be a way to create an index tensor of "actions in set" instead of "action == value"
     # if we do that, actionLoss should also be a min expression instead of a squeeze
     actionLoss = allLoss[:, actions == action].squeeze()
+    # TODO: we could simplify this to an arg min, right? average over to find confidence of best action
     otherLoss = allLoss[:, actions != action].min(axis=1).values
     # count the number of times this action is dominated as the final output
-    return torch.as_tensor(actionLoss > otherLoss, dtype=torch.float).mean().item()
+    return torch.as_tensor(actionLoss <= otherLoss, dtype=torch.float).mean().item()
+
+
+def sampleActionConfidence(dist: Distribution, samples: int, lossFunction: callable, action: int, actions: Tensor
+                           ) -> float:
+    """
+    Samples the probability that the given action dominates all other actions.
+    :param dist:          Distribution to sample
+    :param samples:       Number of samples to take
+    :param lossFunction:  Loss function, first parameter is the Y label (0 or 1) and second is the action tensor
+    :param action:        Primary action to consider
+    :param actions:       Tensor of all possible actions
+    :return:  Probability that action dominates all other actions
+    """
+    return computeActionConfidence(dist.sample(torch.Size((samples,))), lossFunction, action, actions)
+
+
+def computeBestAction(phi: Tensor, lossFunction: callable, actions: Tensor) -> Tuple[int, float]:
+    """
+    Determines the best action for the given phi samples, actions, and loss function.
+    :param phi:           Samples from the phi distribution
+    :param lossFunction:  Loss function, first parameter is the Y label (0 or 1) and second is the action tensor
+    :param actions:       Tensor of all possible actions
+    :return:  Best action, and the probability
+    """
+    bestAction = actions[0]
+    bestConfidence = 0
+    for action in actions:
+        confidence = computeActionConfidence(phi, lossFunction, action, actions)
+        if confidence > bestConfidence:
+            bestAction = action
+            bestConfidence = confidence
+    return int(bestAction), bestConfidence
+
+
+def sampleBestAction(dist: Distribution, samples: int, lossFunction: callable, actions: Tensor) -> Tuple[int, float]:
+    return computeBestAction(dist.sample(torch.Size((samples,))), lossFunction, actions)
+
+
+def computeProbabilityActionDominated(phi: Tensor, lossFunction: callable, action: int, actions: Tensor) -> float:
+    """
+    Computes the probability that the given action is dominated by another action.
+
+    **Deprecated**: use `computeActionConfidence` instead.
+    :param phi:           Samples from the phi distribution
+    :param lossFunction:  Loss function, first parameter is the Y label (0 or 1) and second is the action tensor
+    :param action:        Primary action to consider
+    :param actions:       Tensor of all possible actions
+    :return:  Probability that action is dominated by another action
+    """
+    return 1 - computeActionConfidence(phi, lossFunction, action, actions)
 
 
 def sampleProbabilityActionDominated(dist: Distribution, samples: int, lossFunction: callable, action: int,
                                      actions: Tensor) -> float:
     """
     Samples the probability that the given action is dominated by another action.
+
+    **Deprecated**: use `sampleActionConfidence` instead.
     :param dist:          Distribution to sample
     :param samples:       Number of samples to take
     :param lossFunction:  Loss function, first parameter is the Y label (0 or 1) and second is the action tensor
