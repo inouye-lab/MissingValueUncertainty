d64db90bee2e1d818be1ab9b3b1a37700f45715a
David Burnett <david.b.laughs@gmail.com>
2025-03-24 07:56:27 -0400

    Add helper to evaluate a data loader that doesn't depend on regular feature/label split
    Will be useful with our new weirder loss functions

--- mvu/model/regressor.py
+++ mvu/model/regressor.py
@@ -7,8 +7,7 @@ import torch
 from overrides import override
 from sklearn.linear_model import Ridge
 from torch import Tensor
-from torch.nn import Module, MSELoss, Sigmoid
-from torch.nn.functional import sigmoid
+from torch.nn import Module, MSELoss
 from torch.utils.data import DataLoader
 
 from ..dataset.csv import CsvDataset, CsvDatasetSplits
@@ -44,6 +43,30 @@ class Regressor(SerializerMixin, ABC):
         predicted = self.predict(dataset.features)
         return torch.mean((predicted - dataset.targets) ** 2)
 
+    @staticmethod
+    @torch.no_grad()
+    def evaluateData(data: DataLoader, batchCallback: callable = None) -> Tensor:
+        """
+        Evaluates the model on the given dataset and logs the final MSE
+        :param data:          Data to evaluate
+        :param batchCallback: Runs the loss on the batch. Given batch, returns (batchLoss, batchSamples)
+        :return:  Mean squared error for the dataset
+        """
+        totalLoss = 0
+        seenSamples = 0
+        totalBatches = len(data)
+        startTime = perf_counter()
+        for batchIndex, batch, in enumerate(data):
+            batchLoss, batchSamples = batchCallback(*batch)
+            totalLoss += batchLoss
+            seenSamples += batchSamples
+            print(f"Evaluating regressor batch {batchIndex + 1}/{totalBatches}", end="\r")
+        # this only happens if we have no data
+        if totalBatches == 0:
+            return torch.tensor([0])
+        logging.info(f"Evaluated regressor with data loader in {perf_counter() - startTime:.5f} seconds")
+        return torch.tensor([totalLoss / totalBatches])
+
     @torch.no_grad()
     def evaluateDataloader(self, data: DataLoader, device: torch.device = None, lossFunction: callable = None
                            ) -> Tensor:
@@ -56,25 +79,18 @@ class Regressor(SerializerMixin, ABC):
         """
         if lossFunction is None:
             lossFunction = MSELoss()
-        totalLoss = 0
-        seenSamples = 0
-        totalBatches = len(data)
-        startTime = perf_counter()
-        for batchIndex, (features, targets) in enumerate(data):
+
+        def batchHandler(features: Tensor, targets: Tensor):
             if device is not None:
                 features = features.to(device)
                 targets = targets.to(device)
             predicted = self.predict(features)
             # TODO: bring back per feature loss? would need a custom loss function and to ditch the item call here
             # might at that point want multiple loss function support
-            totalLoss += lossFunction(predicted, targets).item()
-            seenSamples += targets.shape[0]
-            print(f"Evaluating regressor batch {batchIndex + 1}/{totalBatches}", end="\r")
-        # this only happens if we have no data
-        if totalBatches == 0:
-            return torch.tensor([0])
-        logging.info(f"Evaluated regressor with data loader in {perf_counter() - startTime:.5f} seconds")
-        return torch.tensor([totalLoss / totalBatches])
+            loss = lossFunction(predicted, targets).item()
+            return loss, targets.shape[0]
+
+        return Regressor.evaluateData(data, batchHandler)
 
     def evaluateSplits(self, ds: CsvDatasetSplits) -> None:
         """
