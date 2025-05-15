1e98f1d1828cc24ed5a2df46c5224980f9b255a4
David Burnett <david.b.laughs@gmail.com>
2025-05-15 12:09:39 -0400

    Cleanup old experiment scripts and update names for calibrate
    Was looking for a different script and these were in the way

--- scripts/experiment/calibrate_celeba.sh
+++ scripts/experiment/calibrate_celeba.sh
@@ -7,12 +7,12 @@ shift
 source ../../miniconda/bin/activate
 conda activate ./venv
 
-python mvce_dataset.py celeba --output ./results/mvce/top/$feature/ --generator_samples 3 30 \
+python calibrate_mvce.py celeba --output ./results/mvce/top/$feature/ --generator_samples 3 30 \
     --dataset "{
       \"path\": \"../../datasets/CelebAMask/256/img\",
       \"lists_root\": \"datasets/celeba\",
       \"attributes_path\": \"../../datasets/CelebAMask/1024/CelebAMask-HQ-attribute-anno.txt\",
-      \"test_list\": \"test_shuffled_1_of_5.flist\",
+      \"validation_list\": \"validation/split_1_of_5.flist\",
       \"return_index\": true,
       \"targets\": [\"$feature\"]
     }" \
@@ -20,7 +20,7 @@ python mvce_dataset.py celeba --output ./results/mvce/top/$feature/ --generator_
     --classifier_feature $feature \
     --cuda_index 0 \
     --mask top --cache_directory ../../datasets/CelebAMask/cache/256/top_batches \
-    --beta_variance_scales 0.5 0.99 --zero_imputation --zero_variance \
+    --beta_variance_scales 0.5 0.99 --zero_imputation \
     --action_spaces zero-one '{"name":"aleatoric", "constantLoss": 0.4}' --threads 4 --trials 4
 
 #for_feature Mouth_Slightly_Open
