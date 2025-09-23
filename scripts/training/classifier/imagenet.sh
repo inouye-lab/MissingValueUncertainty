e25ea53c1781fb8a117cea8448662fbe4f58d3a5
David Burnett <david.b.laughs@gmail.com>
2025-09-23 19:29:35 -0400

    Add parameter to imagenet training script for iterations
    Useful for some debug mostly

--- scripts/training/classifier/imagenet.sh
+++ scripts/training/classifier/imagenet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cuda=${1:-0}
+iterations=${2:-0}
 shift $#
 
 source ../../miniconda/bin/activate
@@ -8,6 +9,8 @@ conda activate ./venv
 
 # really just loads then saves it and runs some tests
 python learn_neural_network.py imagenet --seed 1337 --output ./models/imagenet/ --cuda_index $cuda \
-    --training_iterations 0 --batch_size 256 --loss CrossEntropy \
+    --scheduler '{ "name": "cosine-annealing", "T_max": 30, "eta_min": 0 }' \
+    --optimizer '{ "name": "sgd", "lr": 0.0001, "weight_decay": 0.0001, "momentum": 0.9 }' \
+    --training_iterations $iterations --validate_every 5 --patience 2 --batch_size 256 --loss CrossEntropy \
     --architecture '{"name": "resnet", "activation": "identity", "keep_final_layer": true}' \
     '{ "path": "../../datasets/ImageNet" }'
